d7900cc65b1d971f2b22a523d8b7785960612787
Hyonsong Lee <hynsngl@gmail.com>
2020-04-16 21:21:39 +0900

    Register base icon as global component
    • Create BaseIcon.vue and define icon name, width, height props
    • Install lodash to refine file names
    • Enable auto global registration of base components
    • Add feather-sprite.svg in public directory

--- src/components/EventCard.vue
+++ src/components/EventCard.vue
@@ -6,6 +6,7 @@
     >
       <span class="eyebrow">@{{ event.time }} on {{ event.date }}</span>
       <h4 class="title">{{ event.title }}</h4>
+      <BaseIcon name="users" />
       <span>{{ event.attendees.length }} attending</span>
     </router-link>
   </div>
